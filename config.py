3c56889a38c141e9b3776f078171f8f1d63f6791
Samar6978 <samar045045@gmail.com>
2024-09-10 12:54:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7259034553:AAHYWdbEpMxeJAkv_HH1QYOChdMLMvot13E")
+BOT_TOKEN=("7188514999:AAE7eAtF5d2vd2Znld15vkusJfiUcktSf88")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("venomXcrazy")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1365601906")
 
 
 
